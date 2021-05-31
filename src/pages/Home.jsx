741319ec4f7ec8cd623b1e499f6f184015951594
unknown <r.loboplaza@hotmail.com>
2021-05-31 20:58:50 -0300

    diseño de cards finalizado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ListaHeroes from '../components/ListaHeroes';
+import '../css/home.css';
 
 const Home = () => {
   return (
     <>
-      <h1>Todos los Personajes</h1>
+      <div className="home-title">
+        <h1>Todos los Personajes</h1>
+      </div>
       <ListaHeroes />
     </>
   );
